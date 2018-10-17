12e961b8380913fb687e1ad3bf70097deac37ca2
JokerWuQAQ <453840856@qq.com>
2018-10-17 23:24:34 +0800

    添加优秀答案

--- easy/two-sum.py
+++ easy/two-sum.py
@@ -44,3 +44,18 @@ class Solution:
                     return [index, nums.index(complement)]
             except:
                 continue
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        优秀答案
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i, num in enumerate(nums):
+            if num in d:
+                return [d[num], i]
+            else:
+                d[target-num] = i
