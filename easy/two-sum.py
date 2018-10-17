a5b1463a4c035f97767a4d1354f09ba48cbf761f
JokerWuQAQ <453840856@qq.com>
2018-10-17 21:40:04 +0800

    官方解答方法二

--- easy/two-sum.py
+++ easy/two-sum.py
@@ -1,7 +1,25 @@
 class Solution:
+    def twoSum(self. nums, target):
+        """
+        首答
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums2 = nums.copy()
+        for index, num1 in enumerate(nums):
+            nums2.remove(num1)
+            num2 = target - num1
+            try:
+                if num2 in nums2:
+                    return [index, nums2.index(num2) + index + 1]
+            except ValueError:
+                continue
+
+class Solution:
     def twoSum(self, nums, target):
         """
-        官方解答的方法一：暴力法
+        官方解答的方法一 暴力法
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
@@ -10,3 +28,19 @@ class Solution:
             for index2, num2 in enumerate(nums[index1 + 1:]):
                 if target - num1 == num2:
                     return [index1, index2 + index1 + 1]
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        官方解答的方法二：两遍哈希表
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for index, num1 in enumerate(nums):
+            complement = target - num1
+            try:
+                if nums.index(complement) != index:
+                    return [index, nums.index(complement)]
+            except:
+                continue
