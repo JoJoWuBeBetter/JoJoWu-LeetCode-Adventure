f272a9a4e1be39475ee7b41867eb0edf73045243
吴姓周先生 <jackking704@gmail.com>
2018-10-27 17:04:15 +0800

    442优秀答案

---
+++ array/442.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        优秀答案
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for x in nums:
+            if nums[abs(x)-1] < 0:
+                res.append(abs(x))
+            else:
+                nums[abs(x)-1] *= -1
+        return res
