9a7bcfa034bc5b43fbb297eeb6659f770af94a58
吴姓周先生 <jackking704@gmail.com>
2018-10-27 16:02:10 +0800

    766 修改后的首答

--- toeplitz_matrix.py
+++ toeplitz_matrix.py
@@ -1,30 +1,32 @@
 class Solution:
-    def isToeplitzMatrix(self, matrix):
-        """
-        首答
-        :type matrix: List[List[int]]
-        :rtype: bool
-        """
-        cont = len(matrix[0])
-        if cont == 1:
-            return True
-        for i ,v in enumerate(matrix):
-            try:
-                if v[:cont-1] != matrix[i + 1][-cont+1:]:
-                    return False
-            except IndexError:
-                pass
-        return True
-
-class Solution:
     def isToeplitzMatrix(self, m):
         """
-        优秀答案
-        :type matrix: List[List[int]]
+        修改后的首答
+        :type m: List[List[int]]
         :rtype: bool
         """
-            for i in range(len(m) - 1):
-                for j in range(len(m[0]) - 1):
-                    if m[i][j] != m[i + 1][j + 1]:
-                        return False
+        cont = len(m[0])
+
+        if cont == 1:
             return True
+
+        for i in range(len(m) - 1):
+            if m[i][:cont - 1] != m[i + 1][-cont+1:]:
+                return False
+
+        return True
+
+
+class Solution:
+    """
+    优秀答案
+    :type m: List[List[int]]
+    :rtype: bool
+    """
+    def isToeplitzMatrix(self, m):
+        for i in range(len(m) - 1):
+            for j in range(len(m[0]) - 1):
+                if m[i][j] != m[i + 1][j + 1]:
+                    return False
+        return True
+
